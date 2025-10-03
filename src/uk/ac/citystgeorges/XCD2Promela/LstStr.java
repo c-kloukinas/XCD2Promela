7316f04bd994c0028ca6b950d0ea97dfde6b407d
c-kloukinas <c.kloukinas@gmail.com>
2025-10-03 15:04:00 +0100

    Basic typedef for ArrayList<String>, to avoid typing it all the time.

---
+++ src/uk/ac/citystgeorges/XCD2Promela/LstStr.java
@@ -0,0 +1,9 @@
+package uk.ac.citystgeorges.XCD2Promela;
+
+import java.util.ArrayList;
+
+// Just a shorthand for ArrayList<String>
+public class LstStr extends ArrayList<String> {
+	LstStr() { super(); }
+	LstStr(int sz) { super(sz); }
+};
