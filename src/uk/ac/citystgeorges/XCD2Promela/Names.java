572716edbc385693f64a26d9e3441458eb118dfd
c-kloukinas <c.kloukinas@gmail.com>
2025-10-13 16:26:46 +0100

    First version - single point of definitions of translated names, to make it easier to find/change them.

---
+++ src/uk/ac/citystgeorges/XCD2Promela/Names.java
@@ -0,0 +1,63 @@
+package uk.ac.citystgeorges.XCD2Promela;
+
+/* serves as a single point of X (big)name definition */
+public class Names {
+    static String True = "true";
+    static String False = "false";
+    static String componentName( String comp ) {
+        return "COMPONENT_"+comp; }
+    static String componentHeaderName( String comp ) {
+        return "proctype instance_name(CompositeName,CompositeID,"
+            + comp + ",CompInstanceID,Instance)"; }
+    static String componentInstanceName( String comp, String inst, String ind) {
+        return "instance_name(CompositeName,CompositeID,"
+            + comp
+            + "," + inst
+            + "," + ind
+            + ")";}
+    static String componentRunInstanceName(String comp,String inst,String ind) {
+        return "  run "+ componentInstanceName(comp, inst, ind) + "();"; }
+    static String componentIConstraintsName(String comp) {
+        return "Component_i_c_code(CompositeName,CompositeID,"
+            + comp
+            + ",CompInstanceID, Instance)"; }
+    static String componentRoleDataName(String comp) {
+        return "Component_i_roleData("
+            + comp
+            + ",CompInstanceID, Instance)"; }
+
+    static String paramName( String comp, String param ) {
+        return "Component_i_Param_N(CompositeName,CompositeID,"
+            + comp + ",CompInstanceID,Instance," + param + ")"; }
+    static String varName( String comp, String var ) {
+        return componentName(comp) + "_VAR_" + var; }
+    static String typeOfVarDefName( String comp, String var ) {
+        return "TYPEOF_" + varName(comp, var); }
+    static String typeOfVar( String var ) {
+        return "TypeOf(" + var + ")"; }
+    static String varPreName( String var ) {
+        return "PRE(" + var + ")"; }
+
+    static String connectorName(String x) {
+        return "CONNECTOR_" + x; }
+
+    static String xVarName( String x, String role, String var ) {
+        return connectorName(x)+"_ROLE_"+role+ "_VAR_"+var; }
+    static String xVarInstanceName(String x,String role,String var,String inst){
+        return xVarName(x,role,var)+inst; }
+
+    static String portActionName( String comp, String port, String act) {
+        return "COMPONENT_" + comp + "_VAR_PORT_" + port + "_ACTION_" + act; }
+    static String portActionNameRes( String comp, String port, String action) {
+        return portActionName(comp, port, action) + "_RESULT"; }
+    static String portActionNameExc( String comp, String port, String action) {
+        return portActionName(comp, port, action) + "_EXCEPTION"; }
+    static String portName( String comp, String port ) {
+        return comp + "_" + port + "_" + BasicVisitor.ln + "_" + BasicVisitor.atchar; }
+
+    static String Void = "XCDVOID";
+    static String Bit = "bit";
+    static String Byte = "byte";
+    static String Short = "short";
+    static String Int = "int";
+}
