3bcae34a46d9b3c3bfc70a8ce7485c8a1cee33b3
c-kloukinas <c.kloukinas@gmail.com>
2025-10-14 16:01:58 +0100

    Added code to process the configuration declaration.

--- src/uk/ac/citystgeorges/XCD2Promela/XCD2PromelaVisitor.java
+++ src/uk/ac/citystgeorges/XCD2Promela/XCD2PromelaVisitor.java
@@ -4,27 +4,31 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 import org.antlr.v4.runtime.*;
 /*
-        ArrayList<String> res = new ArrayList<String>();
-        String s = "";
+  ArrayList<String> res = new ArrayList<String>();
+  String s = "";
 
-        res.add(s);
-        return res;
+  res.add(s);
+  return res;
 
-        var framenow = env.get(env.size()-1);
-        var symbolInfo = getIdInfo(symbol);
+  var framenow = env.get(env.size()-1);
+  var symbolInfo = getIdInfo(symbol);
 */
 
 public class XCD2PromelaVisitor extends ConnectorVisitor {
     XCD2PromelaVisitor() {
-        String compilationUnitID = ""; // root
+        String compilationUnitID = "@root"; // root
         // initialise env, so that "result", etc. are known IDs
         env = new ArrayList<ContextInfo>();
-        env.add(new ContextInfo(compilationUnitID, XCD_type.unknownt, false));
+        env.add(new ContextInfo());
         String array_sz = "";
         String initVal = "";
         String var_prefix = "";
@@ -48,11 +52,58 @@ public class XCD2PromelaVisitor extends ConnectorVisitor {
 
     /*
       Always empty methods
-     */
+    */
     @Override
     public LstStr visitCompilationUnits(XCDParser.CompilationUnitsContext ctx) { return visitChildren(ctx); }
 
     @Override
-    public LstStr visitCompilationUnit(XCDParser.CompilationUnitContext ctx) { return visitChildren(ctx); }
+    public LstStr visitCompilationUnit(XCDParser.CompilationUnitContext ctx) {
+        LstStr res=null;
+        if (ctx.config!=null) {
+            ContextInfo framenow = env.get(env.size()-1); // root
+            ContextInfoComp newctx
+                = framenow.makeContextInfoComp("@configuration", false);
+            env.add(newctx);
+            res=visitChildren(ctx);
+            try (InputStream in
+                 = XCD2Promela.class.getResourceAsStream("/resources/configuration.pml.template");
+                 BufferedReader reader
+                 = new BufferedReader(new InputStreamReader(in));
+                 FileWriter theConfig
+                 = XCD2Promela.mynewoutput("configuration.pml")) {
+                if (newctx.map.size()!=2) {
+                    mywarning("Configuration should have exactly one component instance - instead, it has " + newctx.map.size());
+                    for (var key : newctx.map.keySet()) {
+                        var val=newctx.map.get(key);
+                        System.err.println("Instance \"" + key
+                                           + "\" of component type \"" + val.variableTypeName
+                                           + "\"\n");
+                    }
+                    myassert(false, "");
+                }
+                myassert(newctx.subcomponents.size()==1
+                         , "Configuration should have exactly one component,"
+                         + " instead it has "
+                         + newctx.subcomponents.size());
+                String instName = newctx.subcomponents.get(0);
+                IdInfo compTypeInfo = newctx.map.get(instName);
+                String compType = compTypeInfo.variableTypeName;
+                myassert(compTypeInfo.type == XCD_type.componentt
+                         , "Configuration instance type is not a component"
+                         + "Instance \"" + instName
+                         + "\" of component type \"" + compType
+                         + "\"");
+                String out = reader.lines() // Stream<String>
+                    .collect(Collectors.joining("\n")) // String
+                    .replace("$<compType>", compType);
+                theConfig.write(out);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            res=visitChildren(ctx);
+        }
+        return res;
+    }
 
 }
