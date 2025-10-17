7043ab5d1c2151876ce3e8347acfdaf895638e7e
c-kloukinas <c.kloukinas@gmail.com>
2025-10-17 15:58:21 +0100

    Added getParamName, so each context can return the appropriate parameter name (for component or port or ...). Probably the same is needed for vars?
    Added new types to represent Name, Type, Sig(nature),
    NameTypePair, SeqOfNameTypePairs, EventStructure, and
    MethodStructure.
    Used these to start the coding for ContextInfoCompPort.

--- src/uk/ac/citystgeorges/XCD2Promela/ContextInfo.java
+++ src/uk/ac/citystgeorges/XCD2Promela/ContextInfo.java
@@ -37,6 +37,10 @@ public class ContextInfo {
                            ));
     }
 
+    String getParamName(String param) {
+        return null;
+    }
+
     ContextInfoComp makeContextInfoComp(String compUnitID, boolean is_paramp)
     {   ContextInfoComp res = new ContextInfoComp(compUnitID
                                                   , XCD_type.componentt
@@ -77,19 +81,85 @@ class ContextInfoComp extends ContextInfo {
         consumerprts = new LstStr();
         emitterprts = new LstStr();
     }
+
+    String getParamName(String param) {
+        return Names.paramNameComponent(compilationUnitID, param);
+    }
 }
-class ContextInfoCompPort extends ContextInfo {
-    LstStr events = new LstStr();
-    LstStr methods = new LstStr();
-    Map<String, String> interaction_constraints
-        = new HashMap<String, String>(); // event/method to IC
-    Map<String, String> functional_constraints
-        = new HashMap<String, String>(); // event/method to FC
-    Map<String, LstStr> params
-        = new HashMap<String, LstStr>(); // event/method to params
+class Name {public String name; Name(String n) {name=n;}@Override public String toString(){return name;}}
+class Type {public String type; Type(String t) {type=t;}@Override public String toString(){return type;}}
+class Sig extends ArrayList<Type> { Sig(){super();} }
+class NameTypePair {
+    NameTypePair(Name n, Type t) {name=n; type=t;}
+    public Name name;
+    public Type type;
+    @Override public String toString(){return name.toString() + "," + type.toString();}
+}
+class SeqOfNameTypePairs extends ArrayList<NameTypePair> {
+    SeqOfNameTypePairs() {super();}
+    SeqOfNameTypePairs(int sz) {super(sz);}
+}
+class EventStructure {
+    public Name name;
+    public Sig param_types;
+    // sig is <name, param_types>
+    public SeqOfNameTypePairs full_sig;
+    public LstStr interaction_constraints;
+    public LstStr functional_constraints;
+}
+class MethodStructure extends EventStructure {
+    public Type resultType;
+    LstStr exceptionTypes;
+}
+class ContextInfoCompPort extends ContextInfoComp {
+    /*
+      One may wish to overload events(/methods).
+
+      So we need to use the full event(/method) signature, i.e.,
+      <name, param type...>. The parameter names are not part of the
+      signature. Neither are the return type (methods) or any
+      exceptions the method can throw (events don't return anything by
+      definition, so cannot throw exceptions either), since exceptions
+      are just a kind of (abnormal) return value.
+
+      To represent these, we need:
+      1) Name=String
+      2) Type=String
+      3) Sig=Pair<Name,Tuple<Type...>> // event/method name, param types...
+      4) EventOverloads=Map<Name,Map<Sig, FullSig>> // it's an error if a sig
+                                           // is defined more than once
+
+      Also:
+      5) FullSig=Tuple<Name // event/method name
+      , Tuple<<Pair<Type, Name>... // param types & names
+      , Type // result type
+      , Tuple<Type...>> // exception types
+    */
+    LstStr basicEventNames = new LstStr(); // potentially overloaded
+    LstStr basicMethodNames = new LstStr();
+    // Name 2 Map of <Sig.toString() 2 EventStructure>
+    Map<Name, Map<String, EventStructure>> eventOverloads
+        = new HashMap<Name, Map<String, EventStructure>>();
+    // Name 2 Map of <Sig.toString() 2 MethodStructure>
+    Map<Name, Map<String, MethodStructure>> methodOverloads
+        = new HashMap<Name, Map<String, MethodStructure>>();
+    // Map<String, EventStructure> allEventInfo = new HashMap<String, EventStructure>();
+    // Map<String, String> interaction_constraints
+    //     = new HashMap<String, String>(); // event/method *sig* to IC
+    // Map<String, String> functional_constraints
+    //     = new HashMap<String, String>(); // event/method *sig* to FC
+    // Map<String, LstStr> params
+    //     = new HashMap<String, LstStr>(); // event/method *sig* to params
 
         ContextInfoCompPort(String portname, XCD_type portType
                         , boolean is_paramp, ContextInfo parent) {
         super(portname, portType, is_paramp, parent);
+    }
+
+
+    String getParamName(String param) {
+        return Names.paramNamePort(parent.compilationUnitID // port's component
+                                   // port name is ignored?!?
+                                   , param);
     }
 }
