e83c6f3b87011993b03fa425832ce1b2e6ad560d
c-kloukinas <c.kloukinas@gmail.com>
2025-10-17 19:03:34 +0100

    Added initial support for enum and typedef. More to do (tests not passing).

--- src/uk/ac/citystgeorges/XCD2Promela/XCD2PromelaVisitor.java
+++ src/uk/ac/citystgeorges/XCD2Promela/XCD2PromelaVisitor.java
@@ -54,8 +54,101 @@ public class XCD2PromelaVisitor extends ConnectorVisitor {
       Always empty methods
     */
     @Override
-    public LstStr visitCompilationUnits(XCDParser.CompilationUnitsContext ctx) { return visitChildren(ctx); }
+    public LstStr visitCompilationUnits(XCDParser.CompilationUnitsContext ctx) {
+        LstStr res = visitChildren(ctx);
+        ContextInfo framenow = env.get(env.size()-1);
+        Map<String,IdInfo> map = framenow.map;
+        map.forEach((key, value) -> {
+                if ((value.type == XCD_type.enumt)
+                    || (value.type == XCD_type.typedeft)) {
+                    System.err.println("Creating file for enum/typedef: "
+                                       + key
+                                       + " Value : " + value.variableTypeName);
+                    try (FileWriter typeFile
+                         = XCD2Promela.mynewoutput("TYPE_"+key+".h")) {
+                        typeFile.write(value.variableTypeName);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        return res;
+    }
 
+    @Override
+    public LstStr visitEnumDeclaration(XCDParser.EnumDeclarationContext ctx) {
+        updateln(ctx);
+        ContextInfo framenow = env.get(env.size()-1);
+        Name enumName = new Name(ctx.id.getText());
+        LstStr res = new LstStr();
+        Sig values = new Sig();
+        String s = "";
+
+        s = "#ifndef " + enumName.toString() + "\n"
+            + "#define " + enumName.toString() + "  mtype\n"
+            + "mtype = {\n\t";
+        var constants = ctx.constants;
+        int size = constants.size();
+        myassert(constants!=null && size>0
+                 , "ERROR: An enum type must have values");
+        var name1st = constants.get(0).getText();
+        values.add(new Type(name1st));
+        s += name1st;
+        int i=1;
+        while (i<size) {
+            String name = constants.get(i++).getText();
+            values.add(new Type(name));
+            s += ", " + name;
+        }
+        s += " }\n#endif\n";
+
+        addIdInfo(enumName.toString()
+                  , XCD_type.enumt
+                  , s
+                  , false
+                  , false, ""
+                  , false, ""
+                  , enumName.toString(), ""
+                  , framenow.compilationUnitID);
+        for (var value : values)
+            addIdInfo(value.toString()
+                      , XCD_type.enumt
+                      , false
+                      , false, ""
+                      , false, ""
+                      , value.toString(), ""
+                      , enumName.toString());
+        res.add(s);
+        return res;
+    }
+    @Override
+    public LstStr visitTypeDefDeclaration(XCDParser.TypeDefDeclarationContext ctx) {
+        updateln(ctx);
+        ContextInfo framenow = env.get(env.size()-1);
+        String typedefOf = ctx.replacementOf.getText();
+        String definition = ctx.id.getText();
+        LstStr res = new LstStr();
+        Sig values = new Sig();
+        String s = "";
+
+        s = "#ifndef " + typedefOf + "\n"
+            + "#define " + typedefOf + " " + definition + "\n"
+            + "#endif\n";
+
+        addIdInfo(typedefOf
+                  , XCD_type.typedeft
+                  , s
+                  , false
+                  , false, ""
+                  , false, ""
+                  , typedefOf, ""
+                  , framenow.compilationUnitID);
+        res.add(s);
+        return res;
+    }
+    /*
+     * The configuration is treated here
+     */
     @Override
     public LstStr visitCompilationUnit(XCDParser.CompilationUnitContext ctx) {
         LstStr res=null;
