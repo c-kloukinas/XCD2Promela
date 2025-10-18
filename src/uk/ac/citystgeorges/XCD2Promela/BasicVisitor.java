f24e45b830726e35a5608875898ca9e23feba0e8
c-kloukinas <c.kloukinas@gmail.com>
2025-10-18 01:42:52 +0100

    Global enum/typedefs are now stored in the root frame, so no longer need to search in the root's children for a name - it should be found on the frame stack always.

--- src/uk/ac/citystgeorges/XCD2Promela/BasicVisitor.java
+++ src/uk/ac/citystgeorges/XCD2Promela/BasicVisitor.java
@@ -606,23 +606,23 @@ public abstract
         /*
          * Is it an enum/typedef defined globally?
          */
-        if (null==res) {
-            ContextInfo root = env.get(0);
-            ArrayList<ContextInfo> rootchildren = root.children;
-            boolean found=false;
-            for (ContextInfo chld : rootchildren) { // same id could
-                                                    // have been
-                                                    // defined in
-                                                    // multiple
-                                                    // children?
-                var the_map = chld.map;
-                if (the_map.containsKey(id))
-                    if (!found) {
-                        res=the_map.get(id); found=true;
-                    } else
-                        myassert(false, "Symbol \""+id+"\" defined in multiple children of the root context");
-            }
-        }
+        // if (null==res) {
+        //     ContextInfo root = env.get(0);
+        //     ArrayList<ContextInfo> rootchildren = root.children;
+        //     boolean found=false;
+        //     for (ContextInfo chld : rootchildren) { // same id could
+        //                                             // have been
+        //                                             // defined in
+        //                                             // multiple
+        //                                             // children?
+        //         var the_map = chld.map;
+        //         if (the_map.containsKey(id))
+        //             if (!found) {
+        //                 res=the_map.get(id); found=true;
+        //             } else
+        //                 myassert(false, "Symbol \""+id+"\" defined in multiple children of the root context");
+        //     }
+        // }
         myassert(res!=null, "Symbol \""+id+"\" not found");
         return res;
     }
