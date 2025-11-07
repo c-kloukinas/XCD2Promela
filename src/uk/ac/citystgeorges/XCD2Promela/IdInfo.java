1aef031206f7ea46ab229a536bf0a5d5e89ddf3a
c-kloukinas <c.kloukinas@gmail.com>
2025-11-07 16:48:14 +0000

    Replaced array & initial value info with ArraySizeContext & Variable_initialValueContext values respectively.

--- src/uk/ac/citystgeorges/XCD2Promela/IdInfo.java
+++ src/uk/ac/citystgeorges/XCD2Promela/IdInfo.java
@@ -1,26 +1,24 @@
 package uk.ac.citystgeorges.XCD2Promela;
 
+import uk.ac.citystgeorges.XCD2Promela.XCDParser.*;
+
 public class IdInfo {
     XCD_type type;
     String  variableTypeName = "";
     boolean is_param;
-    boolean is_array;
-    String arraySz;
-    boolean has_initVal;
-    String initVal;
+    ArraySizeContext arraySz;
+    Variable_initialValueContext initVal;
     String big_name;
     String var_prefix;
     String parent;
     IdInfo(XCD_type tp, boolean is_paramp
-           , boolean is_arrayp, String arraySize
-           , boolean has_initValp, String theInitVal
+           , ArraySizeContext arraySize
+           , Variable_initialValueContext theInitVal
            , String big, String prefix
            , String prnt){
         type = tp;
         is_param = is_paramp;
-        is_array = is_arrayp;
         arraySz = arraySize;
-        has_initVal = has_initValp;
         initVal = theInitVal;
         big_name = big;
         var_prefix = prefix;
