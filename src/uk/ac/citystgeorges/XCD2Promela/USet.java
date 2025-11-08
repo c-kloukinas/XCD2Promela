699989d1e826f251712e5e95a08f38c306808461
c-kloukinas <c.kloukinas@gmail.com>
2025-11-08 18:02:38 +0000

    Initial commit - basic set functions.

---
+++ src/uk/ac/citystgeorges/XCD2Promela/USet.java
@@ -0,0 +1,62 @@
+package uk.ac.citystgeorges.XCD2Promela;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class USet<T> {
+
+    @SafeVarargs
+    public static <T> Set<? extends T>
+        setUnion(Collection<? extends T>... sets) {
+        Stream<? extends T> emptyStream = new ArrayList<T>().stream();
+
+        Stream<? extends T> finalStream = emptyStream;
+        int howMany = sets.length;
+        for (int i=0; i<howMany; ++i) {
+            Stream<? extends T> cllctn = sets[i].stream();
+            finalStream = Stream.concat(finalStream, cllctn);
+        }
+
+        return finalStream.collect(Collectors.toSet());
+    }
+
+    @SafeVarargs
+    public static <T> Set<? extends T>
+        setIntersection(Collection<? extends T>... sets) {
+        Stream<? extends T> emptyStream = new ArrayList<T>().stream();
+
+        Stream<? extends T> finalStream = emptyStream;
+        int howMany = sets.length;
+        if (howMany>0)
+            finalStream = Stream.concat(finalStream, sets[0].stream());
+        for (int i=1; i<howMany; ++i) {
+            var theSet = sets[i];
+            finalStream = finalStream.filter( el -> theSet.contains(el) );
+        }
+
+        return finalStream.collect(Collectors.toSet());
+    }
+
+    @SafeVarargs
+    public static <T> Set<? extends T>
+        setDifference(Collection<? extends T>... sets) {
+        Stream<? extends T> emptyStream = new ArrayList<T>().stream();
+
+        Stream<? extends T> finalStream = emptyStream;
+        int howMany = sets.length;
+        if (howMany>0)
+            finalStream = Stream.concat(finalStream, sets[0].stream());
+        for (int i=1; i<howMany; ++i) {
+            var theSet = sets[i];
+            finalStream = finalStream.filter( el -> !theSet.contains(el) );
+        }
+
+        return finalStream.collect(Collectors.toSet());
+    }
+
+}
