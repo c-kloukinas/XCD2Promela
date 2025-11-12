cea32fb4e5466a29d937d75a1cf74fa6dd73b3a9
c-kloukinas <c.kloukinas@gmail.com>
2025-11-12 18:49:09 +0000

    Added new field translation, to store the translation of that construct. Replaces previous field big_name. removed field prefix - never used it.

--- src/uk/ac/citystgeorges/XCD2Promela/IdInfo.java
+++ src/uk/ac/citystgeorges/XCD2Promela/IdInfo.java
@@ -2,26 +2,27 @@ package uk.ac.citystgeorges.XCD2Promela;
 
 import uk.ac.citystgeorges.XCD2Promela.XCDParser.*;
 
-public class IdInfo {
+class IdInfo {
     XCD_type type;
     String  variableTypeName = "";
     boolean is_param;
     ArraySizeContext arraySz;
     Variable_initialValueContext initVal;
-    String big_name;
-    String var_prefix;
+    LstStr translation = new LstStr();
+    // String big_name;
+    // String var_prefix;
     String parent;
     IdInfo(XCD_type tp, boolean is_paramp
            , ArraySizeContext arraySize
            , Variable_initialValueContext theInitVal
-           , String big, String prefix
+           // , String big, String prefix
            , String prnt){
         type = tp;
         is_param = is_paramp;
         arraySz = arraySize;
         initVal = theInitVal;
-        big_name = big;
-        var_prefix = prefix;
+        // big_name = big;
+        // var_prefix = prefix;
         parent = prnt;
     }
 }
