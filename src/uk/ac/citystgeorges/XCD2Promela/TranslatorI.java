38bad7ef675e8622f5eb106363b3c3b64dc0b702
c-kloukinas <c.kloukinas@gmail.com>
2025-11-12 18:46:15 +0000

    Initial commit - an interface for the translation classes/actions of each grammar rule.

---
+++ src/uk/ac/citystgeorges/XCD2Promela/TranslatorI.java
@@ -0,0 +1,11 @@
+package uk.ac.citystgeorges.XCD2Promela;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+
+/**
+ * Goal of this interface is to help split the actual translation code
+ * in separate files, one per the context that it applies to.
+ */
+public interface TranslatorI {
+    public T translate(BaseVisitor<T> bv, ParserRuleContext ctx);
+}
