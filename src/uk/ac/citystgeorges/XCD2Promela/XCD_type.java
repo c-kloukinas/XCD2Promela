606f08301f59523cc4f9460fd1e413810686f095
c-kloukinas <c.kloukinas@gmail.com>
2025-12-01 17:16:58 +0000

    Added mparamt type, for the parameters of methods/events.

--- src/uk/ac/citystgeorges/XCD2Promela/XCD_type.java
+++ src/uk/ac/citystgeorges/XCD2Promela/XCD_type.java
@@ -4,7 +4,7 @@ enum XCD_type {
     unknownt, roott
     , typet
     , voidt, boolt, bytet, intt
-    , paramt, vart
+    , paramt, vart, mparamt
     , resultt, exceptiont
     , emittert, consumert, requiredt, providedt
     , emittervart, consumervart, requiredvart, providedvart
